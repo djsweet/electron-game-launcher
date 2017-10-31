ef87bf5846f39af1355690d0e4e7a0de0ebd988f
Daniel Sweet <danieljsweet@icloud.com>
2017-10-31 19:56:15 -0400

    Performance improvements in tokenizer

--- Electron/libmgr/tokenizer.js
+++ Electron/libmgr/tokenizer.js
@@ -28,19 +28,48 @@
  * }
  */
 
-var sd  = require('string_decoder');
-var err = require('./errors');
+/*
+ * Performance notes, for posterity:
+ * (on Node v6.11.5, subject to change)
+ * const, module-level: good
+ * const, anywhere else: bad
+ * let, anywhere: bad
+ * for .. of: bad
+ * Number instead of String for characters: good
+ * Number instead of String for states: good
+ * Implicit this vs. explicit object: ?
+ */
+
+const sd  = require('string_decoder');
+const err = require('./errors');
+
+const _whitespace = Array(0x80);
+for (var i = 0x00; i < 0x80; i++) {
+    var wsRegex    = /\s/, chr = String.fromCharCode(i);
+    _whitespace[i] = wsRegex.test(chr);
+}
+
+function ccIsWhitespace(cc) {
+    return (cc < 0x80) && _whitespace[cc];
+}
+
+// Mode declarations
+const normalMode  = 0, commentMode = 1, stringMode = 2;
+// "Type" declarations
+const stringType = 'string';
+
 
 function Tokenizer(encoding='utf-8') {
-    this._sd      = new sd.StringDecoder(encoding);
-    this._mode    = 'normal';
-    this._line    = 1;
-    this._col     = 0; // Start at col = 0 so that the += is always right
+    this._sd       = new sd.StringDecoder(encoding);
+    this._mode     = normalMode;
+    this._line     = 1;
+    this._col      = 0; // Start at col = 0 so that the += is always right
     // by the return time
-    this._lastChr = null;
-    this._strLine = 0;
-    this._strCol  = 0;
-    this._strTok  = "";
+    this._lastChr  = null;
+    this._strLine  = 0;
+    this._strCol   = 0;
+    this._strTok   = null;
+    this._strStart = 0;
 }
 
 function readableChar(chr) {
@@ -60,109 +89,161 @@ function readableChar(chr) {
     }
 }
 
+// State actions are separate, non-exported functions so that
+// we can profile them using the built-in V8 profiler, which
+// only samples at the function call level.
+
+Tokenizer.prototype._doNormalChar = function (chr, cc, strOff, oot) {
+    if (0x2f === cc) { // 0x2f == '/'
+        if (this._lastChr === 0x2f) {
+            this._mode = commentMode;
+        }
+        this._lastChr = cc;
+        return true;
+    } else if (this._lastChr === 0x2f) {
+        throw new err.LexError(
+            "Unexpected character after /: " + readableChar(chr),
+            this._line,
+            this._col
+        );
+    }
+    if (0x0a === cc) { // 0x0a == '\n'
+        this._line   += 1;
+        this._col     = 0;
+        this._lastChr = chr;
+        return true;
+    } 
+    if (this._lastChr === 0x0d) { // 0x0d == '\r'
+        throw new err.LexError(
+            "Unexpected character after \\r: " + readableChar(chr),
+            this._line,
+            this._col
+        );
+    }
+    if (0x22 === cc) { // 0x22 == '"'
+        this._mode     = stringMode;
+        this._strLine  = this._line;
+        this._strCol   = this._col;
+        this._strStart = strOff + 1;
+    } else if (!(ccIsWhitespace(cc))) {
+        oot.push({
+            type:   chr,
+            line:   this._line,
+            column: this._col
+        });
+    }
+    return false;
+}
+
+Tokenizer.prototype._doCommentChar = function (cc) {
+    if (0x0a === cc) { // 0x0a == '\n'
+        this._line += 1;
+        this._col   = 0;
+        this._mode  = normalMode;
+    }
+}
+
+Tokenizer.prototype._bumpChars = function (str, stopOff) {
+    if (this._strTok) {
+        this._strTok += str.substring(this._strStart, stopOff);
+    } else {
+        this._strTok  = str.substring(this._strStart, stopOff);
+    }
+    return this;
+}
+
+Tokenizer.prototype._doStringChar = function (cc, str, len, off, oot) {
+    if (0x5c === this._lastChr) { // 0x5c == '\\'
+        // We escaped the current character and have already set it up
+        // to be included in the new substring.
+        if (off === len - 1) {
+            this._bumpChars(str, len);
+        }
+    } else if (0x22 === cc) { // 0x22 == '"'
+        var goingOut;
+        if (this._strTok) {
+            goingOut = this._strTok + str.substring(this._strStart, off);
+        } else {
+            goingOut = str.substring(this._strStart, off);
+        }
+        oot.push({
+            type:   stringType,
+            value:  goingOut,
+            line:   this._strLine,
+            column: this._strCol
+        });
+        this._strTok  = null;
+        this._strLine = 0;
+        this._strCol  = 0;
+        this._mode    = normalMode;
+    } else if (cc !== 0x5c) {
+        // If this._lastChr !== '\\' then we don't want
+        // to drop this current '\\' in, because it's being
+        // used to drop the next one in.
+        if (0x0a === cc) { // 0x0a == '\n'
+            this._line += 1;
+            this._col   = 0;
+        }
+        // By default, we just let the outer loop increment off
+        // Unless we're at the very end of the string, then we have
+        // to save what we've read in for the next pushBuffer
+        if (off === len - 1) {
+            // If there was anything left over from the pushBuffer before
+            // the current pushBuffer call, then _strTok != null.
+            // _strTok != null when we encounter an escape within the same
+            // buffer as well.
+            this._bumpChars(str, len);
+        }
+    } else { // cc === 0x5c == '\\'
+        if (off === 0) {
+            this._strStart += 1;
+        } else {
+            this._bumpChars(str, off);
+            this._strStart = off + 1;
+        }
+    }
+}
 
 Tokenizer.prototype.pushBuffer = function(buf, last = false) {
     var curInput  = (last) ? this._sd.end(buf) : this._sd.write(buf);
+    var cil       = curInput.length;
     var outTokens = [];
-    for (var chr of curInput) {
+    for (var i = 0; i < cil; i++) {
+        var chr    = curInput[i], cc = curInput.charCodeAt(i);
         this._col += 1;
         switch (this._mode) {
-            case 'normal': {
-                if ('/' == chr) {
-                    if (this._lastChr == '/') {
-                        this._mode = 'comment';
-                    }
-                    this._lastChr = chr;
+            case normalMode:
+                if (this._doNormalChar(chr, cc, i, outTokens)) {
                     continue;
-                } else if (this._lastChr == '/') {
-                    throw new err.LexError(
-                        "Unexpected character after /: " + readableChar(chr),
-                        this._line,
-                        this._col
-                    );
                 }
-                if ('\n' == chr) {
-                    this._line   += 1;
-                    this._col     = 0;
-                    this._lastChr = chr;
-                    continue;
-                } 
-                if (this._lastChr == '\r') {
-                    throw new err.LexError(
-                        "Unexpected character after \\r: " + readableChar(chr),
-                        this._line,
-                        this._col
-                    );
-                }
-                if (!(/\s/.test(chr))) {
-                    if ('"' == chr) {
-                        this._mode    = 'string';
-                        this._strLine = this._line;
-                        this._strCol  = this._col;
-                    } else {
-                        outTokens.push({
-                            type:   chr,
-                            line:   this._line,
-                            column: this._col
-                        });
-                    }
-                }
-            }
                 break;
-            case 'comment': {
-                if ('\n' == chr) {
-                    this._line += 1;
-                    this._col   = 0;
-                    this._mode  = 'normal';
-                }
-            }
+            case commentMode:
+                this._doCommentChar(cc);
                 break;
-            case 'string': {
-                if ('\\' == this._lastChr) {
-                    this._strTok += chr;
-                } else if ('"' == chr) {
-                    outTokens.push({
-                        type:   'string',
-                        value:  this._strTok,
-                        line:   this._strLine,
-                        column: this._strCol
-                    });
-                    this._strTok  = "";
-                    this._strLine = 0;
-                    this._strCol  = 0;
-                    this._mode    = 'normal';
-                } else if (chr !== '\\') {
-                    // If this._lastChr !== '\\' then we don't want
-                    // to drop this current '\\' in, because it's being
-                    // used to drop the next one in.
-                    if ('\n' == chr) {
-                        this._line += 1;
-                        this._col   = 0;
-                    }
-                    this._strTok += chr;
-                }
-            }
+            case stringMode:
+                this._doStringChar(cc, curInput, cil, i, outTokens);
                 break;
         }
         // '\\\\' cancels out in string mode;
         // this._lastChr === '\\' isn't even tested in others
         // Because of that, the other modes won't care that we
         // lie about the last character in the event of '\\\\'
-        if (chr === '\\' && this._lastChr === '\\') {
-            this._lastChr = ' ';
+        if (cc === 0x5c && this._lastChr === 0x5c) { // 0x5c === '\\'
+            this._lastChr = 0x20; // 0x20 == ' '
         } else {
-            this._lastChr = chr;
+            this._lastChr = cc;
         }
     }
+    this._strStart = 0;
     return outTokens;
 };
 
 Tokenizer.prototype.maybeFinished = function() {
-    return this._mode != 'string';
+    return this._mode != stringMode;
 };
 
 Tokenizer.prototype.bufferProgress = function() {
-    return {'line': this._line, 'column': this._col};
+    return {line: this._line, column: this._col};
 }
 
 exports.Tokenizer = Tokenizer;
--- Electron/test/libmgr.js
+++ Electron/test/libmgr.js
@@ -1,202 +1,8 @@
 var assert = require('assert');
 var tkn    = require('../libmgr/tokenizer');
+var gen    = require('./gen');
 
-describe('libmgr tokenization', function() {
-    function genString(size) {
-        var ret = "";
-        for (var i = 0; i < size; i++) {
-            var lo   = 0x20;
-            var hi   = 0x7e;
-            var chr  = Math.floor(
-                Math.random() * (hi - lo) + lo
-            );
-            var toChr = String.fromCharCode(chr);
-            ret += toChr;
-        }
-        return ret;
-    }
-
-    function escapeString(str) {
-        var ret = "";
-        for (var chr of str) {
-            if (chr === '"' || chr === '\\') {
-                ret += '\\';
-            }
-            ret += chr;
-        }
-        return ret;
-    }
-
-    function genEntry(
-        sslo, 
-        sshi, 
-        nelo,
-        nehi,
-        recWeight, 
-        depth=0
-    ) {
-        var ret = [];
-        var entries = Math.floor(
-            Math.random() * (nehi - nelo) + nelo
-        );
-        for (var i = 0; i < entries; i++) {
-            var ekSize = Math.floor(
-                Math.random() * (sshi - sslo) + sslo
-            );
-            var entryKey = genString(ekSize);
-            ret.push({
-                item: '"' + escapeString(entryKey) + '"',
-                expect: {
-                    type: 'string',
-                    value: entryKey
-                }
-            });
-            var dp = Math.random();
-            if (dp < recWeight && depth < 3) {
-                var prespec = genEntry(
-                    sslo, 
-                    sshi, 
-                    nelo,
-                    nehi,
-                    recWeight, 
-                    depth+1
-                );
-                ret.push({
-                    item: '{',
-                    expect: {
-                        type: '{'
-                    }
-                });
-                ret.push(...prespec);
-                ret.push({
-                    item: '}',
-                    expect: {
-                        type: '}'
-                    }
-                });
-            } else {
-                var vSize = Math.floor(
-                    Math.random() * (sshi - sslo) + sslo
-                );
-                var value = genString(vSize);
-                ret.push({
-                    item: '"' + escapeString(value) + '"',
-                    expect: {
-                        type: 'string',
-                        value: value
-                    }
-                });
-            }
-        }
-        return ret;
-    }
-
-    function reifyValidData(
-        sslo, 
-        sshi, 
-        nelo, 
-        nehi, 
-        wss = 1,
-        comment = false,
-        recWeight = 0.2
-    ) {
-        var preSpec    = genEntry(sslo, sshi, nelo, nehi, recWeight);
-        var psl        = preSpec.length;
-        var postSpec   = [];
-        var retStrings = [];
-        var line       = 1;
-        var column     = 1;
-        function doPreSpec(cur) {
-            var postExpects = cur.expect;
-            // We have a reference to the pre-gen'd expect;
-            // any modifications we do here affect cur inside preSpec
-            // This is ok, we don't expect to leak preSpec
-            postExpects.line   = line;
-            postExpects.column = column;
-            postSpec.push(postExpects);
-            var psItem         = cur.item;
-            for (var chr of psItem) {
-                if (chr == '\n') {
-                    line  += 1;
-                    column = 1;
-                } else {
-                    column += 1;
-                }
-            }
-            var wstt = Math.floor(
-                Math.random() * (wss + 1)
-            );
-            for (var j = 0; j < wstt; j++) {
-                var wscnf = Math.random();
-                if (wscnf < 0.8) {
-                    psItem += ' ';
-                } else {
-                    psItem += '\t';
-                }
-                column += 1;
-            }
-            return psItem;
-        }
-        for (var i = 0; i < psl; i++) {
-            var basisEntry = doPreSpec(preSpec[i]);
-            if (preSpec[i].item == '}') {
-                // } entries always get their own line
-                if (comment && Math.random() < 0.33) {
-                    basisEntry += "//";
-                    basisEntry += genString(sshi).replace(/\n/, '');
-                }
-                retStrings.push(basisEntry);
-                line  += 1;
-                column = 1;
-                continue;
-            }
-            if (i < (psl - 1)) {
-                i++;
-            } else {
-                continue; // This really shouldn't happen, but might
-            }
-            var doNextOnSameLine = true;
-            if (preSpec[i] == '{' && Math.random() < 0.5) {
-                // Sometimes, { will fall on the next line
-                doNextOnSameLine = false;
-            }
-            if (doNextOnSameLine) {
-                basisEntry += doPreSpec(preSpec[i]);
-                if (comment && Math.random() < 0.25) {
-                    basisEntry += "//";
-                    basisEntry += genString(sshi).replace(/\n/, '');
-                }
-                retStrings.push(basisEntry);
-                line  += 1;
-                column = 1;
-            } else {
-                if (comment && Math.random() < 0.1) {
-                    basisEntry += "//";
-                    basisEntry += genString(sshi).replace(/\n/, '');
-                }
-                retStrings.push(basisEntry);
-                line  += 1;
-                column = 1;
-                if (comment && Math.random() < 0.25) {
-                    retStrings.push("//" + genString(sshi).replace(/\n/, ''));
-                }
-                basisEntry = doPreSpec(preSpec[i]);
-                if (comment && Math.random() < 0.1) {
-                    basisEntry += "//";
-                    basisEntry += genString(sshi).replace(/\n/, '');
-                }
-                retStrings.push(basisEntry);
-                line  += 1;
-                column = 1;
-                if (comment && Math.random() < 0.05) {
-                    retStrings.push("//" + genString(sshi).replace(/\n/, ''));
-                    line += 1;
-                }
-            }
-        }
-        return {basis: retStrings, expects: postSpec};
-    }
-
+describe('libmgr tokenization', function() { 
     function generateValidData() {
         var params = [
             {
@@ -275,7 +81,7 @@ describe('libmgr tokenization', function() {
             } // */
         ];
         return params.map((p) =>
-            reifyValidData(
+            gen.reifyValidData(
                 p.sslo, p.sshi, p.nelo, p.nehi, p.wss, p.comment, p.recWeight
             )
         );
@@ -339,7 +145,7 @@ describe('libmgr tokenization', function() {
                     unixBlocks.forEach(function (blk, idx) {
                         var curLex = unixTkn.pushBuffer(
                             blk, 
-                            idx === unixBlocks.length
+                            idx === (unixBlocks.length - 1)
                         );
                         if (curLex.length) {
                             var cll = curLex.length;
@@ -352,7 +158,7 @@ describe('libmgr tokenization', function() {
                     winBlocks.forEach(function (blk, idx) {
                         var curLex = winTkn.pushBuffer(
                             blk,
-                            idx === unixBlocks.length
+                            idx === (winBlocks.length - 1)
                         );
                         if (curLex.length) {
                             var cll = curLex.length;
